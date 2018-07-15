18199cf18b4c807c5d92a769361617e54ea84fcb
leoiphonedev <aggarwalaman1985@gmail.com>
2018-07-15 15:28:44 +0530

    Create pulse animation in swift

--- PulseAnimation-Swift/ViewController.swift
+++ PulseAnimation-Swift/ViewController.swift
@@ -10,9 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgvAvatar: UIImageView!
+
+    var pulseLayers = [CAShapeLayer]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imgvAvatar.layer.cornerRadius = imgvAvatar.frame.size.width/2.0
+        createPulse()
+    }
+    
+    func createPulse() {
+        for _ in 0...2 {
+            let circularPath = UIBezierPath(arcCenter: .zero, radius: UIScreen.main.bounds.size.width/2.0, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+            let pulseLayer = CAShapeLayer()
+            pulseLayer.path = circularPath.cgPath
+            pulseLayer.lineWidth = 2.0
+            pulseLayer.fillColor = UIColor.clear.cgColor
+            pulseLayer.lineCap = kCALineCapRound
+            pulseLayer.position = CGPoint(x: imgvAvatar.frame.size.width/2.0, y: imgvAvatar.frame.size.width/2.0)
+            imgvAvatar.layer.addSublayer(pulseLayer)
+            pulseLayers.append(pulseLayer)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.animatePulse(index: 0)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                self.animatePulse(index: 1)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.animatePulse(index: 2)
+                }
+            }
+        }
+    }
+    
+    func animatePulse(index: Int) {
+        pulseLayers[index].strokeColor = UIColor.black.cgColor
+
+        let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
+        scaleAnimation.duration = 2.0
+        scaleAnimation.fromValue = 0.0
+        scaleAnimation.toValue = 0.9
+        scaleAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        scaleAnimation.repeatCount = .greatestFiniteMagnitude
+        pulseLayers[index].add(scaleAnimation, forKey: "scale")
+        
+        let opacityAnimation = CABasicAnimation(keyPath: #keyPath(CALayer.opacity))
+        opacityAnimation.duration = 2.0
+        opacityAnimation.fromValue = 0.9
+        opacityAnimation.toValue = 0.0
+        opacityAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        opacityAnimation.repeatCount = .greatestFiniteMagnitude
+        pulseLayers[index].add(opacityAnimation, forKey: "opacity")
+
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
